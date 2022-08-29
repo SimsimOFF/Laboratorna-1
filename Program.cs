3d45b017886850f0fd4a3c8fe86f421154b2c064
{{SimsimOFF}} <{{S.Melik-Kazaryan_FIT_11_21_B_d@knute.edu.ua}}>
2022-08-29 10:07:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorna1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
